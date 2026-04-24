a610fee51d1288334d5e46e3f323230d9692850b
atyagiabhi210 <atyagiabhi210@gmail.com>
2026-04-25 00:13:57 +0530

    feat: implement basic API routes for website management

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,7 +1,20 @@
-use std::fmt::Error;
-#[tokio::main]
+use poem::{get, post, handler, listener::TcpListener, web::Path, Route, Server};
 
-async fn main()-> Result<(), Error>{
-    println!("Hello, world!");
-    Ok(())
+#[handler]
+fn get_website(Path(website_id): Path<String>) -> String {
+    format!("Website ID: {}", website_id)
+}
+#[handler]
+fn create_website(Path(website_id): Path<String>) -> String {
+    format!("Create website with ID: {}", website_id)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new()
+        .at("/website/:website_id", get(get_website))
+        .at("/website", post(create_website));
+    Server::new(TcpListener::bind("0.0.0.0:3001"))
+      .run(app)
+      .await
 }
